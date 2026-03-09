536f7df421a7a22f76b20a668d10fffc92514f27
shubhambashte <shubhamrbashte@gmail.com>
2026-03-09 20:01:08 +0530

    Comments in C

---
+++ Comments in C.c
@@ -0,0 +1,14 @@
+/* 
+
+Comments are hints that we add to our code, making it easier to understand.
+
+*/
+
+#include <stdio.h>
+
+int main() {
+
+  // print Hello World to the screen //
+  printf("Hello! SHUBHAM");
+  return 0;
+}
