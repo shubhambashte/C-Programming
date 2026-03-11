379292b13f74c048acaa693b83895501586667b2
shubhambashte <shubhamrbashte@gmail.com>
2026-03-11 13:16:46 +0530

    Types of Operators

--- Operators.c
+++ Operators.c
@@ -63,7 +63,7 @@ int main(){
 */
 
 // Working of Relational Operators 
-
+/*
 #include <stdio.h>
 int main(){
 
@@ -81,6 +81,54 @@ int main(){
     printf("%d >= %d is %d \n", a, c, a >= c);
     printf("%d <= %d is %d \n", a, b, a <= b);
     printf("%d <= %d is %d \n", a, c, a <= c);
+
+    return 0;
+}
+    */
+
+// Working of Logical Operators 
+/*
+#include <stdio.h>
+int main(){
+
+    int a = 18, b = 45, c= 7, result;
+
+    result = (a==b) && (c > b);
+    printf("(a = = b) && (c > b) is %d\n", result);
+
+    result = (a==b) && (c < b);
+    printf("(a==b) && (c<b) is %d\n", result);
+
+    result = (a==b) || (c<b);
+    printf("(a==b) || (c<b) is %d\n", result);
+    
+    result = (a != b) || (c < b);
+    printf("(a != b) || (c < b) is %d \n", result);
+
+    result = !(a != b);
+    printf("!(a != b) is %d \n", result);
+
+    result = !(a == b);
+    printf("!(a == b) is %d \n", result);
+
+    return 0;
+}
+*/
+
+// sizeof Operator
+
+#include <stdio.h>
+int main(){
+
+    int a;
+    float b;
+    double c;
+    char d;
+
+    printf("Size of int=%lu bytes\n", sizeof(a));
+    printf("Size of float=%lu bytes\n",sizeof(b));
+    printf("Size of double=%lu bytes\n",sizeof(c));
+    printf("Size of char=%lu byte\n",sizeof(d));
 
     return 0;
 }
