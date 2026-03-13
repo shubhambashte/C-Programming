21b6f716a898cf6472aa990d703bc932515b6a20
shubhambashte <shubhamrbashte@gmail.com>
2026-03-13 20:48:39 +0530

    Multidimensional Arrays

---
+++ MultiDimensionalArrays.c
@@ -0,0 +1,119 @@
+// Two-dimensional array to store and print values
+/*
+// C program to store temperature of two cities of a week and display it.
+#include <stdio.h>
+const int CITY = 2;
+const int WEEK = 7;
+int main()
+{
+  int temperature[CITY][WEEK];
+
+  // Using nested loop to store values in a 2d array
+  for (int i = 0; i < CITY; ++i)
+  {
+    for (int j = 0; j < WEEK; ++j)
+    {
+      printf("City %d, Day %d: ", i + 1, j + 1);
+      scanf("%d", &temperature[i][j]);
+    }
+  }
+  printf("\nDisplaying values: \n\n");
+
+  // Using nested loop to display vlues of a 2d array
+  for (int i = 0; i < CITY; ++i)
+  {
+    for (int j = 0; j < WEEK; ++j)
+    {
+      printf("City %d, Day %d = %d\n", i + 1, j + 1, temperature[i][j]);
+    }
+  }
+  return 0;
+}
+
+// Sum of two matrices
+
+// C program to find the sum of two matrices of order 2*2
+
+#include <stdio.h>
+int main()
+{
+  float a[2][2], b[2][2], result[2][2];
+
+  // Taking input using nested for loop
+  printf("Enter elements of 1st matrix\n");
+  for (int i = 0; i < 2; ++i)
+    for (int j = 0; j < 2; ++j)
+    {
+      printf("Enter a%d%d: ", i + 1, j + 1);
+      scanf("%f", &a[i][j]);
+    }
+
+  // Taking input using nested for loop
+  printf("Enter elements of 2nd matrix\n");
+  for (int i = 0; i < 2; ++i)
+    for (int j = 0; j < 2; ++j)
+    {
+      printf("Enter b%d%d: ", i + 1, j + 1);
+      scanf("%f", &b[i][j]);
+    }
+
+  // adding corresponding elements of two arrays
+  for (int i = 0; i < 2; ++i)
+    for (int j = 0; j < 2; ++j)
+    {
+      result[i][j] = a[i][j] + b[i][j];
+    }
+
+  // Displaying the sum
+  printf("\nSum Of Matrix:");
+
+  for (int i = 0; i < 2; ++i)
+    for (int j = 0; j < 2; ++j)
+    {
+      printf("%.1f\t", result[i][j]);
+
+      if (j == 1)
+        printf("\n");
+    }
+  return 0;
+}
+
+*/
+// Three-dimensional array
+
+// C Program to store and print 12 values entered by the user
+
+#include <stdio.h>
+int main()
+{
+  int test[2][3][2];
+
+  printf("Enter 12 values: \n");
+
+  for (int i = 0; i < 2; ++i)
+  {
+    for (int j = 0; j < 3; ++j)
+    {
+      for (int k = 0; k < 2; ++k)
+      {
+        scanf("%d", &test[i][j][k]);
+      }
+    }
+  }
+
+  // Printing values with the proper index.
+
+  printf("\nDisplaying values:\n");
+  for (int i = 0; i < 2; ++i)
+  {
+    for (int j = 0; j < 3; ++j)
+    {
+      for (int k = 0; k < 2; ++k)
+      {
+        printf("test[%d][%d][%d] = %d\n", i, j, k, test[i][j][k]);
+      }
+    }
+  }
+
+  return 0;
+}
