cbd77b4ed49929d368a41566cd61fdc9adccf5a8
shubhambashte <shubhamrbashte@gmail.com>
2026-03-15 19:28:10 +0530

    Structures and Pointers

---
+++ StructsandPointers.c
@@ -0,0 +1,70 @@
+// Access members using Pointer
+/*
+#include <stdio.h>
+struct person
+{
+   int age;
+   float weight;
+};
+
+int main()
+{
+    struct person *personPtr, person1;
+    personPtr = &person1;   
+
+    printf("Enter age: ");
+    scanf("%d", &personPtr->age);
+
+    printf("Enter weight: ");
+    scanf("%f", &personPtr->weight);
+
+    printf("Displaying:\n");
+    printf("Age: %d\n", personPtr->age);
+    printf("weight: %f", personPtr->weight);
+
+    return 0;
+}
+    */
+
+
+// Dynamic memory allocation of structs
+/*
+#include <stdio.h>
+#include <stdlib.h>
+struct person {
+   int age;
+   float weight;
+   char name[30];
+};
+
+int main()
+{
+   struct person *ptr;
+   int i, n;
+
+   printf("Enter the number of persons: ");
+   scanf("%d", &n);
+
+   // allocating memory for n numbers of struct person
+   ptr = (struct person*) malloc(n * sizeof(struct person));
+
+   for(i = 0; i < n; ++i)
+   {
+       printf("Enter first name and age respectively: ");
+
+       // To access members of 1st struct person,
+       // ptr->name and ptr->age is used
+
+       // To access members of 2nd struct person,
+       // (ptr+1)->name and (ptr+1)->age is used
+       scanf("%s %d", (ptr+i)->name, &(ptr+i)->age);
+   }
+
+   printf("Displaying Information:\n");
+   for(i = 0; i < n; ++i)
+       printf("Name: %s\tAge: %d\n", (ptr+i)->name, (ptr+i)->age);
+
+   return 0;
+}
+*/
+
