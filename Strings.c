4fb90696519099346171c57463091348f2f5ab0f
shubhambashte <shubhamrbashte@gmail.com>
2026-03-15 12:55:40 +0530

    Strings in C

---
+++ Strings.c
@@ -0,0 +1,66 @@
+// scanf() to read a string
+/*
+#include <stdio.h>
+int main()
+{
+    char name[20];
+    printf("Enter name: ");
+    scanf("%s", name);
+    printf("Your name is %s.", name);
+    return 0;
+}
+*/
+/*
+//fgets() and puts()
+
+#include <stdio.h>
+int main()
+{
+    char name[30];
+    printf("Enter name: ");
+    fgets(name, sizeof(name), stdin);  // read string
+    printf("Name: ");
+    puts(name);    // display string
+    return 0;
+}
+    */
+
+    /*
+// Passing string to a Function
+
+#include <stdio.h>
+void displayString(char str[]);
+
+int main()
+{
+    char str[50];
+    printf("Enter string: ");
+    fgets(str, sizeof(str), stdin);             
+    displayString(str);     // Passing string to a function.    
+    return 0;
+}
+void displayString(char str[])
+{
+    printf("String Output: ");
+    puts(str);
+}
+    */
+
+// Strings and Pointers
+
+#include <stdio.h>
+
+int main(void) {
+  char name[] = "Harry Potter";
+
+  printf("%c", *name);     // Output: H
+  printf("%c", *(name+1));   // Output: a
+  printf("%c", *(name+7));   // Output: o
+
+  char *namePtr;
+
+  namePtr = name;
+  printf("%c", *namePtr);     // Output: H
+  printf("%c", *(namePtr+1));   // Output: a
+  printf("%c", *(namePtr+7));   // Output: o
+}
