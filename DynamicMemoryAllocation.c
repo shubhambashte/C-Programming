c127606a55776b4b4be1fcd0f01e90be46282b6c
shubhambashte <shubhamrbashte@gmail.com>
2026-03-15 12:24:51 +0530

    Types of Dynamic Memory Allocation

---
+++ DynamicMemoryAllocation.c
@@ -0,0 +1,98 @@
+// C Dynamic Memory Allocation
+
+// malloc() and free()
+
+// Program to calculate the sum of n numbers entered by the user
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int n, i, *ptr, sum = 0;
+
+  printf("Enter number of elements: ");
+  scanf("%d", &n);
+
+  ptr = (int*) malloc(n * sizeof(int));
+ 
+  // if memory cannot be allocated
+  if(ptr == NULL) {
+    printf("Error! memory not allocated.");
+    exit(0);
+  }
+
+  printf("Enter elements: ");
+  for(i = 0; i < n; ++i) {
+    scanf("%d", ptr + i);
+    sum += *(ptr + i);
+  }
+
+  printf("Sum = %d", sum);
+  
+  // deallocating the memory
+  free(ptr);
+
+  return 0;
+}
+*/
+/*
+// calloc() and free()
+
+// Program to calculate the sum of n numbers entered by the user
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int n, i, *ptr, sum = 0;
+  printf("Enter number of elements: ");
+  scanf("%d", &n);
+
+  ptr = (int*) calloc(n, sizeof(int));
+  if(ptr == NULL) {
+    printf("Error! memory not allocated.");
+    exit(0);
+  }
+
+  printf("Enter elements: ");
+  for(i = 0; i < n; ++i) {
+    scanf("%d", ptr + i);
+    sum += *(ptr + i);
+  }
+
+  printf("Sum = %d", sum);
+  free(ptr);
+  return 0;
+}
+*/
+
+// realloc()
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int *ptr, i , n1, n2;
+  printf("Enter size: ");
+  scanf("%d", &n1);
+
+  ptr = (int*) malloc(n1 * sizeof(int));
+
+  printf("Addresses of previously allocated memory:\n");
+  for(i = 0; i < n1; ++i)
+    printf("%pc\n",ptr + i);
+
+  printf("\nEnter the new size: ");
+  scanf("%d", &n2);
+
+  // rellocating the memory
+  ptr = realloc(ptr, n2 * sizeof(int));
+
+  printf("Addresses of newly allocated memory:\n");
+  for(i = 0; i < n2; ++i)
+    printf("%pc\n", ptr + i);
+  
+  free(ptr);
+
+  return 0;
+}
